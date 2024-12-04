46fb77ab9d1de223fc0efef20e72797c01c32000
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-04 11:10:28 +0500

    Initial Commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import css from './Header.module.css';
+const Header = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+
+      </div>
+
+      <div className={css.right}>
+        <div className={css.menu}>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
