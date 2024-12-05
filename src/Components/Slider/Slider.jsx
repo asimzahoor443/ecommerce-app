5a06de940b5329a22caff27beaea9b8e1af9d958
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-05 13:01:15 +0500

    initial updates

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -6,15 +6,18 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './Slider.css';
 import { SliderProducts } from '../../data/products';
-// import swiper styles
 const Slider = () => {
   return (
     <div className="s-container">
       <Swiper
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+        navigation={true}
+        loopFillGroupWithBlank={true}
         slidesPerView={3}
         spaceBetween={40}
         slidesPerGroup={1}
-        loop={false}
+        loop={true}
       >
         {SliderProducts.map((slide, i) => (
           <SwiperSlide>
